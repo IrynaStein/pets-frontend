94d532c05a548455329e7932f757640d368aca6e
Iryna Stein <isophiestein@gmail.com>
2021-08-26 17:35:49 -0600

    set up userauth/login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import './App.css';
 import { Switch, Route } from "react-router-dom"
 import Login from './Login'
 import Signup from './Signup';
+import User from './User';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  console.log(user)
+
+  if (!user) return <Login onLogin={setUser}/>;
+
   return (
     <Switch>
-      <Route exact path="/">
-        <Login />
+      <Route path="/">
+        <User user={user} onLogout={setUser}/>
       </Route>
       <Route path="/signup">
         <Signup />
