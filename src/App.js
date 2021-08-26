86224b83a0bf28ceb46aeea6fed3fc43a7be7a70
Iryna Stein <isophiestein@gmail.com>
2021-08-26 10:12:56 -0600

    initialstyling of welcome page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Petz</h1>
+      <form>
+      <input className="login-input" placeholder="user name..."></input>
+      <br/>
+      <input className="login-input" placeholder="password..."></input>
+      <br/>
+      <button>Login</button>
+      <button>Signup</button>
+      </form>
     </div>
   );
 }
