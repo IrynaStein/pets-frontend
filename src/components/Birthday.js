4596c5e5fbc00e307e284ce436cb664a3d21685e
Iryna Stein <isophiestein@gmail.com>
2021-08-31 15:11:05 -0600

    n

---
+++ src/components/Birthday.js
@@ -0,0 +1,30 @@
+export default function Birthday({pet}){
+    console.log(pet)
+    const today = new Date().getDate();
+    // const date = [(today.getMonth() + 1), today.getDate()]
+    const age = pet.birthday.split("-").slice(1);
+  
+    const currentAge = today - age[1];
+  
+    console.log(age);
+    console.log(today);
+    console.log(currentAge);
+  
+    function image() {
+      switch (currentAge) {
+        case 0:
+          return pet.breed.age[0].image;
+        case 1:
+          return pet.breed.age[1].image;
+        case 2:
+          return pet.breed.age[2].image;
+        case 3:
+          return pet.breed.age[3].image;
+        default:
+          return pet.breed.age[3].image;
+      }
+    }
+    return (
+        <img src={image()} alt ="pet"/>
+    )
+}
--- src/pages/GameContainer.js
+++ src/pages/GameContainer.js
@@ -1,23 +1,29 @@
 import "./GameContainer.css";
 import { useParams } from "react-router-dom";
 import { useSelector} from "react-redux";
+import Birthday from "../components/Birthday";
+
 
 export default function GameContainer() {
   const params = useParams();
   console.log(params.petName);
-  const pets = useSelector(state => state.petList)
+  const pets = useSelector(state => state.petList).filter(pet => pet.name === params.petName)
   console.log(pets)
 
-  const currentPet = pets
-  .filter(pet => pet.name === params.petName)
-  console.log(currentPet)
-  const {name, breed, activity, birthday, food, sleepy, bored, healthy, hungry} = currentPet
-
+//   const currentPet = pets
+//   .filter(pet => pet.name === params.petName)
+  console.log(pets)
+  const {name, breed, activity, birthday, food, sleepy, bored, healthy, hungry} = pets
+console.log(birthday)
   return (
     <div className="game-container">
       <p>Game Container</p>
-      <p>{params.petName}</p>
-      <img ></img>
+      <p>{name}</p>
+      <div className="pet-display">
+      <Birthday pet={pets}/>
+      </div>
+      <p>{breed}</p>
+      <p>{activity}</p>
     </div>
   );
 }
