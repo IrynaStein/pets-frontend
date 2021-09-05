8e8182ef9e7d7e6fe1a7e72bff034afd9be11803
Iryna Stein <isophiestein@gmail.com>
2021-09-05 13:57:53 -0600

    added passed pets map to dispaly tomstones

--- src/components/Cemetery.js
+++ src/components/Cemetery.js
@@ -1,17 +1,21 @@
 import './Cemetery.css'
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import {fetchCemetery} from '../store/gameSlice'
 export default function Cemetery(){
 
     const dispatch = useDispatch()
+    const passedPets = useSelector(state => state.game.cemetery)
+
     useEffect(() => {
        dispatch(fetchCemetery())
-    }, [])
+    }, [dispatch])
 
     return (
         <div className="cemetery-container">
         <h1>Cemetery</h1>
+        {passedPets.map((pet) => <div className="tombstone" key={pet.id}><img src="https://live.staticflickr.com/65535/51426137870_96aedb2582_o.png" alt="tombsotone"/>{pet.name}</div>)}
         </div>
     )
 }
+
