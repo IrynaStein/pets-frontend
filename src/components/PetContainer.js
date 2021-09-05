fbe5db10ebb0f8df69cbe425e9329e4e812f5a05
Iryna Stein <isophiestein@gmail.com>
2021-09-05 13:28:54 -0600

    deleted all logic for ciemetery

--- src/components/PetContainer.js
+++ src/components/PetContainer.js
@@ -5,12 +5,11 @@ import { useSelector } from "react-redux";
 
 export default function PetsContainer() {
   const pets = useSelector((state) => state.pets.petList);
+  console.log(pets)
   const renderPets = () =>
-    pets.map((pet) => {
-      if (pet.alive) {
-        <Pet key={pet.id} pet={pet} />;
-      }
-    });
+    pets.map((pet) => 
+     <Pet key={pet.id} pet={pet} />
+    );
 
   return (
     <div className="pet-container">
