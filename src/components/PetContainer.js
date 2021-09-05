3958967b2aa0b7c7553dc0f8ad242556eecf95a3
Iryna Stein <isophiestein@gmail.com>
2021-09-04 19:14:24 -0600

    added logic to filter dead pets

--- src/components/PetContainer.js
+++ src/components/PetContainer.js
@@ -5,11 +5,20 @@ import { useSelector } from "react-redux";
 
 export default function PetsContainer() {
   const pets = useSelector((state) => state.pets.petList);
-  const renderPets = () => pets.map((pet) => <Pet key={pet.id} pet={pet} />);
+  const renderPets = () =>
+    pets.map((pet) => {
+      if (pet.alive) {
+        <Pet key={pet.id} pet={pet} />;
+      }
+    });
 
   return (
     <div className="pet-container">
-      {pets.length <= 2 ? <Link to="/create_pet"><button className="button-n">Create new pet</button></Link> : null}
+      {pets.length <= 2 ? (
+        <Link to="/create_pet">
+          <button className="button-n">Create new pet</button>
+        </Link>
+      ) : null}
       {renderPets()}
     </div>
   );
