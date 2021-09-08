522c71e0fbc26415ffb236cc1f676836fb281ac3
Iryna Stein <isophiestein@gmail.com>
2021-09-08 14:37:59 -0600

    updated user upload form to useFrom and also added logic for skipping an avatar update

--- src/components/User.js
+++ src/components/User.js
@@ -9,6 +9,7 @@ export default function User() {
   const [showForm, setShowForm] = useState(false);
   const user = useSelector((state) => state.user.user);
   const { user_name, email, avatar, id, image } = user;
+//   debugger;
   const errorsBe = useSelector((state) => state.user.errors);
   const preloadedValues = {
     user_name: user_name,
@@ -38,6 +39,11 @@ export default function User() {
     }
   };
 
+  const handleCancel = () => {
+      reset()
+      setShowForm(false)
+  }
+
   return (
     <>
       {showForm ? (
@@ -50,6 +56,7 @@ export default function User() {
                 </div>
               ))
             : null}
+            <div className="button-regular-inv" onClick={handleCancel} style={{fontSize: "15px"}}> &#8592;Back to profile</div>
           <form onSubmit={handleSubmit(onSubmit)}>
             <input
               className="input-field-gray"
@@ -94,12 +101,6 @@ export default function User() {
                 Update my profile
               </button>
               <br />
-              <button
-                className="button-gray"
-                onClick={() => setShowForm(false)}
-              >
-                Cancel
-              </button>
             </div>
           </form>
         </div>
@@ -114,7 +115,7 @@ export default function User() {
               Delete profile
             </button>
           </div>
-          <img className="user-avatar" src={image.url} alt="user" />
+          <img className="user-avatar" src={image !==null ? image.url : "https://live.staticflickr.com/65535/51434875121_54db17d433_o.png"} alt="user" />
           <p>Hello {user_name}!</p>
           <p>
             You can create or edit your profile here. Below is the list of pets
--- src/pages/SignupForm.js
+++ src/pages/SignupForm.js
@@ -9,172 +9,185 @@ function SignupForm() {
   const dispatch = useDispatch();
   const errors = useSelector((state) => state.user.errors);
   const history = useHistory();
-  // const errors = useSelector((state) => state.user.errors);
-  // console.log(errors);
-  // const { register, handleSubmit, reset } = useForm();
+  
+  const { register, handleSubmit } = useForm();
 
-  // const onFileChange = (e) => {
-  //   console.log(e.target.files[0])
-  // }
 
-  // const onSubmit = (data, e) => {
-
-  //   console.log(data);
-  //   e.preventDefault();
-  //   dispatch(createUser(data));
-  //   if (errors === "") {
-  //    history.push("/home");
-  //   } else {
-  //     reset();
-  //   }
-  // };
-  // return (
-  //   <div className="App">
-  //     <Link className="button-regular" to="login">
-  //       &#8592;Back to Login
-  //     </Link>
-
-  //     <form className="centered-form-signup" onSubmit={handleSubmit(onSubmit)}>
-  //       <input
-  //         className="input-field-orange"
-  //         name="user-name"
-  //         placeholder="user name..."
-  //         {...register("user_name", {required: true})}
-  //       ></input>
-  //       <br />
-  //       <input
-  //         className="input-field-orange"
-  //         name="password"
-  //         type="password"
-  //         placeholder="password..."
-  //         {...register("password", {required: true})}
-  //       ></input>
-  //       <br />
-  //       <input
-  //         className="input-field-orange"
-  //         name="password_confirmation"
-  //         type="password"
-  //         placeholder="confirm password..."
-  //         {...register("password_confirmation", {required: true})}
-  //       ></input>
-  //       <br />
-  //       <input
-  //         className="input-field-orange"
-  //         name="email"
-  //         placeholder="email..."
-  //         {...register("email", {required: true})}
-  //       ></input>
-  //       <br />
-  //       <input className="input-field-orange" type="file" name="avatar" {...register("avatar")} ></input>
-  //       <br />
-  //       <button className="button-regular" type="submit" >
-  //         Signup</button>
-  //       {errors}
-  //     </form>
-
-  //   </div>
-  // );
-  const defaultForm = {
-    user_name: "",
-    password: "",
-    password_confirmation: "",
-    email: "",
-    image: "",
-  };
-  const [formData, setFormdData] = useState(defaultForm);
-  console.log(errors);
-  // debugger;
-  function handleChange(e) {
-    console.log(e.target.value);
-    setFormdData({ ...formData, [e.target.name]: e.target.value });
-  }
-  function handleFileUpload(e) {
-    console.log(e.target.value);
-    setFormdData({ ...formData, image: e.target.files[0] });
-  }
-
-  function handleSubmit(e) {
+  const onSubmit = (data, e) => {
+    console.log(data.image[0]);
     e.preventDefault();
-    console.log(formData);
-    const fD = new FormData();
-    // debugger;
-    fD.append("image", formData.image);
-    fD.append("user_name", formData.user_name);
-    fD.append("password", formData.password);
-    fD.append("password_confirmation", formData.password_confirmation);
-    fD.append("email", formData.email);
-    // dispatch(createUser(formData))
-    // .then(errors.length === 0 ? history.push("/home") : () => setFormdData(defaultForm))
-    fetch("/signup", {
-      method: "POST",
-      credentials: "include",
-      body: fD,
-    });
-  }
+    const formData = new FormData();
+    if (data.image[0]){
+      formData.append("image", data.image[0]);
+      formData.append("user_name", data.user_name);
+      formData.append("password", data.password);
+      formData.append("password_confirmation", data.password_confirmation);
+      formData.append("email", data.email);
+    } 
+    else {
+      formData.append("user_name", data.user_name);
+      formData.append("password", data.password);
+      formData.append("password_confirmation", data.password_confirmation);
+      formData.append("email", data.email);
+    }
+      dispatch(createUser(formData));
+      history.push("/home");
+  };
+
+
   return (
     <div className="App">
-      <Link to="login">
-        <button className="button-regular">&#8592;Back to Login</button>
+      <Link className="button-regular" to="login">
+        &#8592;Back to Login
       </Link>
-      <br />
-      {errors.length > 0
-        ? errors[0].map((err, ind) => (
-            <div style={{ fontSize: "12px" }}>
-              {ind + 1}. {err}
-              <br />
-            </div>
-          ))
-        : null}
-      <form className="centered-form-signup" onSubmit={handleSubmit}>
+
+      <form enctype="multipart/form-data" className="centered-form-signup" onSubmit={handleSubmit(onSubmit)}>
         <input
           className="input-field-orange"
-          name="user_name"
-          value={formData.user_name}
-          onChange={(e) => handleChange(e)}
+          name="user-name"
           placeholder="user name..."
+          {...register("user_name", {required: true})}
         ></input>
         <br />
         <input
           className="input-field-orange"
-          type="password"
           name="password"
-          value={formData.password}
-          onChange={(e) => handleChange(e)}
+          type="password"
           placeholder="password..."
+          {...register("password", {required: true})}
         ></input>
         <br />
         <input
           className="input-field-orange"
-          type="password"
           name="password_confirmation"
-          value={formData.password_confirmation}
-          onChange={(e) => handleChange(e)}
+          type="password"
           placeholder="confirm password..."
+          {...register("password_confirmation", {required: true})}
         ></input>
         <br />
         <input
           className="input-field-orange"
           name="email"
-          value={formData.email}
-          onChange={(e) => handleChange(e)}
           placeholder="email..."
+          {...register("email", {required: true})}
         ></input>
         <br />
-        <input
-          id="fileupload"
-          type="file"
-          name="image"
-          onChange={(e) => handleFileUpload(e)}
-          placeholder="email..."
-          hidden
-        ></input>
-        <label for="fileupload">Add image</label>
+        <input className="input-field-orange" type="file" name="image" {...register("image")}></input>
+        
         <br />
-        <button className="button-regular-inv" type="submit">
-          Signup
-        </button>
+        <button className="button-regular" type="submit" >
+          Signup</button>
+        {errors}
       </form>
+
     </div>
   );
+
+
+
+
+  
+  // const defaultForm = {
+  //   user_name: "",
+  //   password: "",
+  //   password_confirmation: "",
+  //   email: "",
+  //   image: "",
+  // };
+  // const [formData, setFormdData] = useState(defaultForm);
+  // console.log(errors);
+  // // debugger;
+  // function handleChange(e) {
+  //   console.log(e.target.value);
+  //   setFormdData({ ...formData, [e.target.name]: e.target.value });
+  // }
+  // function handleFileUpload(e) {
+  //   console.log(e.target.value);
+  //   setFormdData({ ...formData, image: e.target.files[0] });
+  // }
+
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   console.log(formData);
+  //   const fD = new FormData();
+  //   // debugger;
+  //   fD.append("image", formData.image);
+  //   fD.append("user_name", formData.user_name);
+  //   fD.append("password", formData.password);
+  //   fD.append("password_confirmation", formData.password_confirmation);
+  //   fD.append("email", formData.email);
+  //   // dispatch(createUser(formData))
+  //   // .then(errors.length === 0 ? history.push("/home") : () => setFormdData(defaultForm))
+  //   fetch("/signup", {
+  //     method: "POST",
+  //     credentials: "include",
+  //     body: fD,
+  //   });
+  // }
+  // return (
+  //   <div className="App">
+  //     <Link to="login">
+  //       <button className="button-regular">&#8592;Back to Login</button>
+  //     </Link>
+  //     <br />
+  //     {errors.length > 0
+  //       ? errors[0].map((err, ind) => (
+  //           <div style={{ fontSize: "12px" }}>
+  //             {ind + 1}. {err}
+  //             <br />
+  //           </div>
+  //         ))
+  //       : null}
+  //     <form className="centered-form-signup" onSubmit={handleSubmit}>
+  //       <input
+  //         className="input-field-orange"
+  //         name="user_name"
+  //         value={formData.user_name}
+  //         onChange={(e) => handleChange(e)}
+  //         placeholder="user name..."
+  //       ></input>
+  //       <br />
+  //       <input
+  //         className="input-field-orange"
+  //         type="password"
+  //         name="password"
+  //         value={formData.password}
+  //         onChange={(e) => handleChange(e)}
+  //         placeholder="password..."
+  //       ></input>
+  //       <br />
+  //       <input
+  //         className="input-field-orange"
+  //         type="password"
+  //         name="password_confirmation"
+  //         value={formData.password_confirmation}
+  //         onChange={(e) => handleChange(e)}
+  //         placeholder="confirm password..."
+  //       ></input>
+  //       <br />
+  //       <input
+  //         className="input-field-orange"
+  //         name="email"
+  //         value={formData.email}
+  //         onChange={(e) => handleChange(e)}
+  //         placeholder="email..."
+  //       ></input>
+  //       <br />
+  //       <input
+  //         id="fileupload"
+  //         type="file"
+  //         name="image"
+  //         onChange={(e) => handleFileUpload(e)}
+  //         placeholder="email..."
+  //         hidden
+  //       ></input>
+  //       <label for="fileupload">Add image</label>
+  //       <br />
+  //       <button className="button-regular-inv" type="submit">
+  //         Signup
+  //       </button>
+  //     </form>
+    // </div>
+  // );
 }
 export default SignupForm;
